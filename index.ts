604309f03c79fa8f2313a37bec018b9d683437ed
Jake Browning <jake.reid.browning@gmail.com>
2023-06-08 00:28:55 +0100

    Restructure

--- index.ts
+++ index.ts
@@ -13,37 +13,52 @@ const getDependencies = (ident: string) => {
 
     const dependantStack = [ident];
 
-    while (dependantStack.length) {
-        const currentIdent = dependantStack.pop();
+    const walkFileAST = (ast: any, currentIdent: string, fn: string) => {
+        const stack = [];
+        walk(ast,
+            {
+                enter(node, parent) {
+                    if (node.type === "Identifier" && node.name === currentIdent) {
+                        while (stack.length) {
+                            const { ancestor, ancestorParent } = stack.pop();
+                            if (ancestorParent.type === "Program") {
+                                stack.length = 0;
+                            }
+                            if (
+                                (
+                                    ancestor.type === "ClassDeclaration" ||
+                                    ancestor.type === "FunctionDeclaration" ||
+                                    ancestor.type === "VariableDeclarator"
+                                )
+                                && ancestor.id.name !== currentIdent
+                            ) {
+                                dependants.push({
+                                    name: ancestor.id.name,
+                                    type: ancestor.type,
+                                    file: fn,
+                                    uses: currentIdent,
+                                });
+                                dependantStack.push(ancestor.id.name);
+                            }
+                        }
+                    }
+                    stack.push({ ancestor: node, ancestorParent: parent });
+                }
+            }
+        );
+    };
 
+    const traverseFileList = (currentIdent: string) => {
         for (const fn of fileList) {
             const content = fs.readFileSync(fn, "utf-8");
             const ast = parseSync(content) as any;
-            const stack = [];
-
-            walk(ast,
-                {
-                    enter(node, parent) {
-                        if (node.type === "Identifier" && node.name === currentIdent) {
-                            while (stack.length) {
-                                const { ancestor, ancestorParent } = stack.pop();
-                                if (ancestorParent.type === "Program") {
-                                    stack.length = 0;
-                                }
-                                if (
-                                    (ancestor.type === "ClassDeclaration" || ancestor.type === "FunctionDeclaration" || ancestor.type === "VariableDeclarator")
-                                    && ancestor.id.name !== currentIdent
-                                ) {
-                                    dependants.push({name: ancestor.id.name, type: ancestor.type, file: fn, uses: currentIdent});
-                                    dependantStack.push(ancestor.id.name);
-                                }
-                            }
-                        }
-                        stack.push({ ancestor: node, ancestorParent: parent });
-                    }
-                }
-            );
+            walkFileAST(ast, currentIdent, fn);
         }
+    };
+
+    while (dependantStack.length) {
+        const currentIdent = dependantStack.pop();
+        traverseFileList(currentIdent);
     }
     return dependants
 };
