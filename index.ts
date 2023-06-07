14c6e434082892744ccffe744afc9eb2ae382998
Jake Browning <jake.reid.browning@gmail.com>
2023-06-07 23:33:57 +0100

    support arbitrary depth in flat array result

--- index.ts
+++ index.ts
@@ -13,37 +13,44 @@ const isVariableDeclarator = (node, ident) => node.type === "VariableDeclarator"
 const getDependencies = (ident: string) => {
     const dependants = [];
 
-    for (const fn of fileList) {
-        const content = fs.readFileSync(fn, "utf-8");
-        const ast = parseSync(content) as any;
-        const stack = [];
+    const dependantStack = [ident];
 
-        walk(ast,
-            {
-                enter(node, parent) {
-                    if (node.type === "Identifier" && node.name === ident) {
-                        while (stack.length) {
-                            const { ancestor, ancestorParent } = stack.pop();
-                            if (ancestorParent.type === "Program") {
-                                stack.length = 0;
-                            }
-                            if (
-                                (ancestor.type === "ClassDeclaration" || ancestor.type === "FunctionDeclaration" || ancestor.type === "VariableDeclarator")
-                                && ancestor.id.name !== ident
-                            ) {
-                                dependants.push({name: ancestor.id.name, type: ancestor.type, node: ancestor, file: fn});
+    while (dependantStack.length) {
+        const currentIdent = dependantStack.pop();
+
+        for (const fn of fileList) {
+            const content = fs.readFileSync(fn, "utf-8");
+            const ast = parseSync(content) as any;
+            const stack = [];
+
+            walk(ast,
+                {
+                    enter(node, parent) {
+                        if (node.type === "Identifier" && node.name === currentIdent) {
+                            while (stack.length) {
+                                const { ancestor, ancestorParent } = stack.pop();
+                                if (ancestorParent.type === "Program") {
+                                    stack.length = 0;
+                                }
+                                if (
+                                    (ancestor.type === "ClassDeclaration" || ancestor.type === "FunctionDeclaration" || ancestor.type === "VariableDeclarator")
+                                    && ancestor.id.name !== currentIdent
+                                ) {
+                                    dependants.push({name: ancestor.id.name, type: ancestor.type, node: ancestor, file: fn});
+                                    dependantStack.push(ancestor.id.name);
+                                }
                             }
                         }
+                        stack.push({ ancestor: node, ancestorParent: parent });
                     }
-                    stack.push({ ancestor: node, ancestorParent: parent });
                 }
-            }
-        );
+            );
+        }
     }
     return dependants
 };
 
 
-const deps = getDependencies("Clazz");
+const deps = getDependencies("MY_CONST");
 // console.dir(deps, { depth: 4 });
 console.log(deps);
