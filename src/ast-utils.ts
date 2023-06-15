aac9c9ee507fea83f3fd5b8670fb2a8334c6e9b8
Jake Browning <jake.reid.browning@gmail.com>
2023-06-15 21:38:37 +0100

    move src into src/

---
+++ src/ast-utils.ts
@@ -0,0 +1,155 @@
+import { types as t } from "@babel/core";
+
+const callMemberExpression = (
+    member: any,
+    property: string,
+    args: any[] = []
+): any =>
+    /**
+     * Calling an object member, e.g. `this.initProps()`
+     */
+    t.expressionStatement(
+        t.callExpression(
+            t.memberExpression(member, t.identifier(property)),
+            args
+        )
+    );
+
+export const buildClassAST = (path: any) => {
+    /**
+     * Builds the class wrapper which returns `__[clas-name]` internally, e.g.
+     *
+     * ```
+     * class A {
+     *     constructor() {
+     *         __class = new __A;
+     *         __class.initProps();
+     *         __class.ctor();
+     *         return __class;
+     *     }
+     * }
+     * ```
+     */
+    const { node } = path;
+    const constr = node.body.body.find((n) => n.key?.name === "constructor");
+
+    return t.classDeclaration(
+        // Prefix with `__$TRANSFORMED__` then remove later, as for some reason babel
+        // throws a "duplicate name" error when swapping a class with one of the same name
+        t.identifier(`__$TRANSFORMED__${node.id.name}`),
+        null,
+        t.classBody([
+            t.classMethod(
+                "constructor",
+                t.identifier("constructor"),
+                constr ? [...constr.params] : [],
+                t.blockStatement([
+                    t.variableDeclaration("var", [
+                        t.variableDeclarator(
+                            t.identifier("__class"),
+                            t.newExpression(
+                                t.identifier(`__${node.id.name}`),
+                                []
+                            )
+                        ),
+                    ]),
+                    callMemberExpression(t.identifier("__class"), "initProps"),
+                    callMemberExpression(
+                        t.identifier("__class"),
+                        "ctor",
+                        constr ? [...constr.params] : []
+                    ),
+                    t.returnStatement(t.identifier("__class")),
+                ])
+            ),
+        ])
+    );
+};
+
+export const buildUnderscoredClassAST = (path: any) => {
+    /**
+     * Builds the underscored class `__[class-name]` for each class, e.g.
+     *
+     * ```
+     * class A extends Base {
+     *     foo = "bar";
+     *
+     *     constructor() {
+     *         console.log(this.foo);
+     *     }
+     * }
+     * ```
+     *
+     * Becomes:
+     *
+     * ```
+     * class __A extends __Base {
+     *     ctor() {
+     *         super.ctor();
+     *         console.log(this.foo);
+     *     }
+     *
+     *     initProps() {
+     *         super.initProps();
+     *         this.foo = "bar";
+     *     }
+     * }
+     * ```
+     */
+    const { node } = path;
+    const constr = node.body.body.find((n) => n.key?.name === "constructor");
+
+    const classProps = path
+        .get("body")
+        .get("body") // `body` array of a `ClassBody` node
+        .filter((p) => t.isClassProperty(p))
+        .map((p) => p.node);
+
+    const superCtorCall = node.superClass
+        ? callMemberExpression(t.super(), "ctor")
+        : t.emptyStatement();
+
+    const superInitCall = node.superClass
+        ? callMemberExpression(t.super(), "initProps")
+        : t.emptyStatement();
+
+    const ctorBlock = t.blockStatement([superCtorCall]);
+    const initBlock = t.blockStatement([superInitCall]);
+
+    // Push constructor class properties to `initProps()`,
+    // e.g. `foo = "bar"` in the class body becomes `this.foo = "bar"` in `initProps()`
+    initBlock.body.push(
+        ...classProps.map((p) =>
+            t.expressionStatement(
+                t.assignmentExpression(
+                    "=",
+                    t.memberExpression(t.thisExpression(), p.key),
+                    p.value === null ? t.nullLiteral() : p.value
+                )
+            )
+        )
+    );
+
+    // Push original class constructor properties (except `super`) to underscored class `ctor()`
+    if (constr) {
+        ctorBlock.body.push(
+            ...constr.body.body.filter(
+                (b) => b.expression?.callee?.type !== "Super"
+            )
+        );
+    }
+
+    return t.classDeclaration(
+        t.identifier(`__${node.id.name}`),
+        node.superClass ? t.identifier(`__${node.superClass.name}`) : null,
+        t.classBody([
+            t.classMethod(
+                "method",
+                t.identifier("ctor"),
+                constr ? [...constr.params] : [],
+                ctorBlock
+            ),
+            t.classMethod("method", t.identifier("initProps"), [], initBlock),
+        ])
+    );
+};
---
+++ src/index.ts
@@ -0,0 +1,84 @@
+import { transformSync, types as t } from "@babel/core";
+import { buildUnderscoredClassAST, buildClassAST } from "./ast-utils";
+import fs from "fs";
+
+const content = fs.readFileSync("demo/nested.ts", "utf-8");
+
+// Array of transformed class names used for determining
+// whether to transform a `prototype` access or `instanceof` expression
+const seen = [];
+
+function myCustomPlugin() {
+    return {
+        visitor: {
+            ClassDeclaration(path) {
+                /**
+                 * Create equivalent `__[class-name]` (underscored) class for each class in the program, and transform the
+                 * original class to a container which returns the underscored version
+                 */
+                const { node } = path;
+
+                if (
+                    node.id.name.startsWith("__") ||
+                    seen.includes(node.id.name)
+                ) {
+                    return;
+                }
+
+                const underscoredClassName = `__${node.id.name}`;
+
+                if (!seen.includes(underscoredClassName)) {
+                    // Insert the underscored class before original class
+                    path.insertBefore(buildUnderscoredClassAST(path));
+                    seen.push(underscoredClassName);
+                }
+
+                // Replace original class with wrapper that returns underscored class
+                path.replaceWith(buildClassAST(path));
+                path.node.id.name = path.node.id.name.replace(
+                    "__$TRANSFORMED__",
+                    ""
+                );
+
+                seen.push(node.id.name);
+            },
+
+            BinaryExpression(path) {
+                /**
+                 * Set `o instanceof [obj-name]` to `o instanceof __[obj-name]` where applicable
+                 */
+                const {
+                    node: { right, operator },
+                } = path;
+                if (operator === "instanceof" && seen.includes(right.name)) {
+                    path.get("right").replaceWith(
+                        t.identifier(`__${right.name}`)
+                    );
+                }
+            },
+
+            MemberExpression(path) {
+                /**
+                 * Set `[obj-name].prototype` to `__[obj-name].prototype` where applicable
+                 */
+                const { node } = path;
+                // If the object has an equivalent`__[obj-name]` and is accessing prototype,
+                // change the prototype's object from `[pbj-name]` to `__[obj-name]`
+                if (
+                    node.property.name === "prototype" &&
+                    seen.includes(node.object.name)
+                ) {
+                    path.get("object").replaceWith(
+                        t.identifier(`__${node.object.name}`)
+                    );
+                }
+            },
+        },
+    };
+}
+
+const output = transformSync(content, {
+    plugins: [myCustomPlugin],
+});
+
+fs.writeFileSync("./dist.js", output.code);
